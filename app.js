85d4c83e4a3af55e8cd9b6df4c1bd9f3ca13900a
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2024-12-13 21:39:59 +0530

    Q2 solved and api written

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express")
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-app.listen(port, ()=> {
+app.get('v1/api/check-aadhaar-status', (req, res) => {
+	const isAadhaar = true;
+
+	if (isAadhaar) {
+		res.status(200).json({
+			message: 'working',
+		});
+	} else {
+		res.status(503).json({
+			message: 'not_working',
+		});
+	}
+});
+app.listen(port, () => {
 	console.log(`App is listening on port:${port}`);
 })
